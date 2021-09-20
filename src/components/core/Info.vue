5c06e9265569f24d1fe05aac384c778006eb3c82
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2021-09-20 15:11:20 +0200

    create Info component

---
+++ src/components/core/Info.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="app-info">
+    <slot />
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Info",
+});
+</script>
+
+<style lang="scss" scoped>
+.app-info {
+  background-color: $darkSecondaryBackground;
+  border: 1px solid $darkBorderColor;
+  color: $grayText;
+  padding: 16px;
+  border-radius: 16px;
+  font-size: 14px;
+}
+</style>

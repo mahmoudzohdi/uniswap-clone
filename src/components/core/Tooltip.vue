92d5300105f08915c1a9b4e4af4335f8644fee6d
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2021-09-22 08:00:49 +0200

    create Tooltip & InfoTooltip components

---
+++ src/components/core/Tooltip.vue
@@ -0,0 +1,104 @@
+<template>
+  <span
+    class="tooltip-holder"
+    ref="tooltip-holder"
+    @mouseenter="mouseenterHandler"
+    @mouseleave="mouseleaveHandler"
+  >
+    <slot />
+  </span>
+  <p
+    :class="['tooltip-content', { show: showTooltip }]"
+    :style="{
+      top: `${tooltipHolderPosition.top + tooltipHolderPosition.height}px`,
+      left: `${tooltipHolderPosition.left + tooltipHolderPosition.width / 2}px`,
+    }"
+  >
+    {{ text }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Tooltip",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      tooltipHolderPosition: {},
+      showTooltip: false,
+      debounceTimeout: null,
+    };
+  },
+  methods: {
+    mouseenterHandler() {
+      this.showTooltip = true;
+    },
+    mouseleaveHandler() {
+      this.showTooltip = false;
+    },
+    updateTooltipHolderPosition() {
+      this.tooltipHolderPosition = this.$refs[
+        "tooltip-holder"
+      ].getBoundingClientRect();
+    },
+    windowResizeHandler() {
+      clearTimeout(this.debounceTimeout);
+      this.debounceTimeout = setTimeout(this.updateTooltipHolderPosition, 200);
+    },
+  },
+  mounted() {
+    this.updateTooltipHolderPosition();
+  },
+  created() {
+    window.addEventListener("resize", this.windowResizeHandler);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.windowResizeHandler);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.tooltip-holder {
+  cursor: default;
+}
+.tooltip-content {
+  position: fixed;
+  transform: translateX(-50%);
+
+  max-width: 220px;
+  display: none;
+
+  font-size: 16px;
+  padding: 10px 16px;
+  border-radius: 12px;
+  background-color: $darkBackground;
+  color: $grayText;
+  font-weight: 400;
+  border: 1px solid $darkBorderColor;
+  z-index: 1;
+  &:before,
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    border-bottom: 9px solid $darkBackground;
+    border-right: 9px solid transparent;
+    border-left: 9px solid transparent;
+  }
+  &:before {
+    border-bottom-color: $darkBorderColor;
+    border-width: 10px;
+  }
+  &.show {
+    display: block;
+  }
+}
+</style>
---
+++ src/components/shared/InfoTooltip.vue
@@ -0,0 +1,38 @@
+<template>
+  <Tooltip :text="text">
+    <span class="question-mark-icon">?</span>
+  </Tooltip>
+</template>
+
+<script>
+import Tooltip from "@/components/core/Tooltip.vue";
+export default {
+  name: "InfoTooltip",
+  components: {
+    Tooltip,
+  },
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.question-mark-icon {
+  width: 18px;
+  height: 18px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  border-radius: 50%;
+  background-color: $darkBorderColor;
+  color: $grayText;
+  &:hover {
+    opacity: 0.7;
+  }
+}
+</style>

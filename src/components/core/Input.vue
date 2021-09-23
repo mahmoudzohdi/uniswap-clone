d5c801f49695679ffd73481c9bb723cddfe78169
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2021-09-23 20:25:53 +0200

    create Input.vue component

---
+++ src/components/core/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <input type="text" class="app-input" v-bind="$attrs" @input="inputHandler" />
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    modelValue: {
+      type: String,
+    },
+  },
+  methods: {
+    inputHandler(e) {
+      this.$emit("update:modelValue", e.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-input {
+  width: 100%;
+  background: none;
+  padding: 16px;
+  border-radius: 20px;
+  border: 1px solid $darkBorderSecondaryColor;
+  font-size: 18px;
+  box-sizing: border-box;
+  &:focus {
+    outline: none;
+    color: #fff;
+  }
+}
+</style>
